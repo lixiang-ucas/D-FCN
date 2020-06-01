eddfea5b382c0631acd4da754fe6968f59849b69
lixiang <1522565816@qq.com>
2020-06-01 21:11:07 +0800

    Update tf_pointSIFT_compile.sh

--- tf_utils/tf_ops/pointSIFT_op/tf_pointSIFT_compile.sh
+++ tf_utils/tf_ops/pointSIFT_op/tf_pointSIFT_compile.sh
@@ -2,7 +2,7 @@ TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
 TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 #/bin/bash
-/usr/local/cuda-8.0/bin/nvcc DFCN.cu -o DFCN_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+/usr/local/cuda-8.0/bin/nvcc pointSIFT.cu -o pointSIFT_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
 
 # TF1.4
-g++ -std=c++11 main.cpp DFCN_g.cu.o -o tf_DFCN_so.so -shared -fPIC -I$TF_INC -I /usr/local/cuda-8.0/include -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 main.cpp pointSIFT_g.cu.o -o tf_pointSIFT_so.so -shared -fPIC -I$TF_INC -I /usr/local/cuda-8.0/include -I$TF_INC/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
