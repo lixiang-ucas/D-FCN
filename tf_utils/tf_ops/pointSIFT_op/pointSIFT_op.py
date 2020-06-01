c3e64753958cd6a8d7ed284b6eab1d8b11e08f25
lixiang <1522565816@qq.com>
2020-06-01 21:16:23 +0800

    Update pointSIFT_op.py

--- tf_utils/tf_ops/pointSIFT_op/pointSIFT_op.py
+++ tf_utils/tf_ops/pointSIFT_op/pointSIFT_op.py
@@ -1,7 +1,7 @@
 """
 Author: Jiang Mingyang
 email: jmydurant@sjtu.edu.cn
-DFCN module op, do not modify it !!!
+pointSIFT module op, do not modify it !!!
 """
 
 import tensorflow as tf
@@ -11,52 +11,52 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 
-DFCN_module = tf.load_op_library(os.path.join(BASE_DIR, 'tf_DFCN_so.so'))
+pointSIFT_module = tf.load_op_library(os.path.join(BASE_DIR, 'tf_pointSIFT_so.so'))
 
-def DFCN_select(xyz, radius):
+def pointSIFT_select(xyz, radius):
     """
     :param xyz: (b, n, 3) float
     :param radius: float
     :return: (b, n, 8) int
     """
-    idx = DFCN_module.cube_select(xyz, radius)
+    idx = pointSIFT_module.cube_select(xyz, radius)
     return idx
 
 
 ops.NoGradient('CubeSelect')
 
-def DFCN_select_two(xyz, radius):
+def pointSIFT_select_two(xyz, radius):
     """
     :param xyz: (b, n, 3) float
     :param radius:  float
     :return: idx: (b, n, 16) int
     """
-    idx = DFCN_module.cube_select_two(xyz, radius)
+    idx = pointSIFT_module.cube_select_two(xyz, radius)
     return idx
 
 
 ops.NoGradient('CubeSelectTwo')
 
-def DFCN_select_four(xyz, radius):
+def pointSIFT_select_four(xyz, radius):
     """
     :param xyz: (b, n, 3) float
     :param radius:  float
     :return: idx: (b, n, 32) int
     """
-    idx = DFCN_module.cube_select_four(xyz, radius)
+    idx = pointSIFT_module.cube_select_four(xyz, radius)
     return idx
 
 
 ops.NoGradient('CubeSelectFour')
 
 
-def DFCN_select_eight(xyz, radius):
+def pointSIFT_select_eight(xyz, radius):
     """
     :param xyz: (b, n, 3) float
     :param radius:  float
     :return: idx: (b, n, 32) int
     """
-    idx = DFCN_module.cube_select_eight(xyz, radius)
+    idx = pointSIFT_module.cube_select_eight(xyz, radius)
     return idx
 
 
